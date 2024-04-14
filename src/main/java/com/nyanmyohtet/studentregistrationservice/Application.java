508aa40021ec8d7153d1ff2a8b2442c4b8d0e318
nyanmyohtet <nyanmyohtet.bx@gmail.com>
2024-04-14 11:23:11 +0630

    feat: Change Application/Test.java

--- src/main/java/com/nyanmyohtet/studentregistrationservice/Application.java
+++ src/main/java/com/nyanmyohtet/studentregistrationservice/Application.java
@@ -2,12 +2,14 @@ package com.nyanmyohtet.studentregistrationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
-public class StudentRegistrationServiceApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StudentRegistrationServiceApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
