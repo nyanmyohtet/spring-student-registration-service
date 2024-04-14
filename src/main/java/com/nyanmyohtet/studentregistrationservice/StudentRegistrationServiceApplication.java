a6335fdf0c940d74d8bb92f788bc6d2f1618c165
nyanmyohtet <nyanmyohtet.bx@gmail.com>
2024-04-14 10:53:53 +0630

    Initial commit

---
+++ src/main/java/com/nyanmyohtet/studentregistrationservice/StudentRegistrationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nyanmyohtet.studentregistrationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentRegistrationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentRegistrationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nyanmyohtet/studentregistrationservice/StudentRegistrationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nyanmyohtet.studentregistrationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentRegistrationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
