f1e68f941495e5153302e58da123254f363bffa9
nyanmyohtet <nyanmyohtet.bx@gmail.com>
2024-04-16 10:48:52 +0630

    feat: Finalize AuthRestController

--- src/main/java/com/nyanmyohtet/studentregistrationservice/api/rest/AuthRestController.java
+++ src/main/java/com/nyanmyohtet/studentregistrationservice/api/rest/AuthRestController.java
@@ -4,6 +4,8 @@ import com.nyanmyohtet.studentregistrationservice.api.request.AuthRequest;
 import com.nyanmyohtet.studentregistrationservice.api.response.AuthResponse;
 import com.nyanmyohtet.studentregistrationservice.persistence.model.User;
 import com.nyanmyohtet.studentregistrationservice.util.JwtTokenUtil;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -25,21 +27,26 @@ public class AuthRestController {
     @Autowired
     JwtTokenUtil jwtUtil;
 
+    private static final Logger logger = LogManager.getLogger(AuthRestController.class);
+
     @PostMapping("/auth/login")
     public ResponseEntity<?> login(@RequestBody @Valid AuthRequest request) {
         try {
+            // Authenticate user
             Authentication authentication = authManager.authenticate(
                     new UsernamePasswordAuthenticationToken(
                             request.getEmail(), request.getPassword())
             );
 
+            // Extract user details from authentication
             User user = (User) authentication.getPrincipal();
+
             String accessToken = jwtUtil.generateAccessToken(user);
             AuthResponse response = new AuthResponse(user.getEmail(), accessToken);
 
-            return ResponseEntity.ok().body(response);
-
+            return new ResponseEntity<>(response, HttpStatus.OK);
         } catch (BadCredentialsException ex) {
+            logger.error(ex.getMessage());
             // TODO: thrown custom ex
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         }
